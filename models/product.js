9aef93081a1cb28dc1498876df4df38a6fb47183
Sheff02 <sheffalisuri@gmail.com>
2020-04-26 21:48:47 +0530

    Schema using mongoose

---
+++ models/product.js
@@ -0,0 +1,41 @@
+const mongoose=require("mongoose")
+const {ObjectId}=mongoose.Schema;
+
+const productschema =  new mongoose.Schema
+({
+name: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength:32,
+    unique:true},
+description:{
+    type: String,
+    trim: true,
+    required: true,
+    maxlength:2000
+},
+price:{
+    type: Number,
+    required: true,
+    trim: true,
+    maxlength:32},
+category:
+{
+    type:ObjectId,
+    ref:"Category",
+    required:true
+},
+stock:{
+    type:Number
+},
+sold:{
+    type:Number,
+    default:0
+},
+photo:{
+    data :Buffer,
+    contentType:String}
+},{timestamps:true})
+
+module.exports=mongoose.model("Product",productSchema);
