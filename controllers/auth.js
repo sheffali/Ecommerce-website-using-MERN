15ca5f7f4faff7fdf6f26f20ec23855a0a7c5133
Sheff02 <sheffalisuri@gmail.com>
2020-05-21 01:10:12 +0530

    Post request using post man

---
+++ controllers/auth.js
@@ -0,0 +1,11 @@
+exports.signup = (req,res) =>{
+   res.json({
+       mesage: "signup rout works"
+   })
+};
+
+exports.signout = (req,res) =>{
+    res.json({
+        message : "signout using json"
+    });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var router = express.Router()
+const {signout,signup} = require("../controllers/auth")
 
-router.get("/signout",(req,res)=>{
-    res.send("signout")
-});
+router.post("/signup",signup);
+router.get("/signout",signout);
 
 module.exports = router;
