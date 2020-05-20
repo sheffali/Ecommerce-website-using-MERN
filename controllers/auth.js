ab4959a44c3a71e492b95787424649665bd1472c
Sheff02 <sheffalisuri@gmail.com>
2020-05-21 02:14:59 +0530

    validations + postman checks

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,30 @@
+const User = require("../models/user")
+const { check, validationResult } = require('express-validator');
+
 exports.signup = (req,res) =>{
-   res.json({
-       mesage: "signup rout works"
-   })
+    
+const errors=validationResult(req)
+if(!errors.isEmpty()){
+    return res.status(422).json({
+        error: errors.array()[0].msg
+
+    });
+}
+    
+    const user = new User(req.body)
+    user.save((err,user) => {
+        if(err){
+            return res.status(400).json({
+                err: "Not able to save user in DB"
+            });
+        }
+        res.json({
+            name: user.name,
+            email: user.email,
+            id: user._id
+        });
+    })
+    
 };
 
 exports.signout = (req,res) =>{
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,13 @@
+const { check, validationResult } = require('express-validator');
 var express = require('express')
 var router = express.Router()
 const {signout,signup} = require("../controllers/auth")
 
-router.post("/signup",signup);
+router.post("/signup",[
+    check("name","name should be atleast 3 char").isLength({min :3}),
+    check("email","email required").isEmail(),
+    check("password","passord atleast 4 ").isLength({min: 4})
+],signup);
 router.get("/signout",signout);
 
 module.exports = router;
