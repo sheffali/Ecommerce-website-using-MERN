665eb5b1ae4e28411ac30212c7a885468720779f
Sheff02 <sheffalisuri@gmail.com>
2020-06-01 01:40:01 +0530

    User controllers

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const Order = require("../models/order");
+
 
 exports.getUserById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
@@ -13,7 +15,6 @@ exports.getUserById = (req, res, next, id) => {
 };
 
 exports.getUser = (req, res) => {
-  //TODO: get back here for password
   req.profile.salt=undefined
   req.profile.encry_password=undefined
   req.profile.createdAt=undefined
@@ -21,14 +22,65 @@ exports.getUser = (req, res) => {
   return res.json(req.profile);
 };
 
-exports.getAllUser=(req,res) =>
+exports.updateUser=(req,res)=>
 {
-    User.find().exec((err,users)=>{
-        if (err || !users) {
-            return res.status(400).json({
-              error: "No user was found in DB"
-            });
-          }
-           res.json(users)
+  User.findByIdAndUpdate(
+    {_id : req.profile._id},
+    {$set: req.body},
+    {new: true, useFindAndModify:false},
+    (err,user) => {
+if(err)    
+{ 
+  return res.statue(400).json({
+    error:"update unsuccessfull"})}  
+user.salt=undefined
+user.encry_password=undefined
+res.json(user);
+    }
+  )
+}
+
+exports.userPurchaseList=(req,res)=>
+{
+  Order.find({user:req.profile._id})
+  .populate("user","_id name")
+  .exec((err,order)=>
+  {
+    if(err)
+    {
+      return res.status(400).json({
+        error : "no order for this acc"
+      })
+    }
+    return res.json(order);
+  });
+}
+
+exports.pushOrderInPurchaseList =(req,res,next)=>{
+  let purchase = []
+  req.body.order.products.forEach(product=>{
+purchase.push({
+  _id:product._id,
+  name:product.name,
+  description:product.description,
+  category:product.category,
+  quantity:product.quantity,
+  amount:req.body.order.amount,
+  transaction_id:req.body.order.transaction_id
+}); });
+//store this in db
+
+User.findOneAndUpdate(
+  { _id:req.profile._id},
+  {$push: {purchase:purchases}},
+  {new: true},//from db send me back the updated object
+(err,purchases) => {
+  if(err){
+    return res.status(400).json({
+      error : "no order for this acc"
     })
 }
+next();
+}
+)
+};
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserById, getUser ,getAllUser} = require("../controllers/user");
+const { getUserById, getUser,updateUser,userPurchaseList} = require("../controllers/user");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
 
@@ -9,6 +9,8 @@ router.param("userId", getUserById);
 
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 
-router.get("/users/",getAllUser);
+router.put("/user/:userId",isSignedIn, isAuthenticated,updateUser);
+
+router.get("/orders/user/:userId",isSignedIn, isAuthenticated,userPurchaseList);
 
 module.exports = router;
